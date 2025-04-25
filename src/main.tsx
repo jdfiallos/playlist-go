59b243a80c7d747a1cccd77ebb5ef81461d29524
Juan Daniel Fiallos <juandanielfiallos@gmail.com>
2025-04-24 21:09:16 -0600

    Refactor App component and styles; remove unused code and integrate Tailwind CSS

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./App.css"; // o tu tailwind import
+import "./index.css"; // o tu tailwind import
+import { ThemeProvider } from "./components/ui/theme-provider"; // si usás dark/light themes
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
